16c46a3d4a51aca26503f08d4d98a11d1f6bda8e
kanciro <medinavargasjhondeivy@gmail.com>
2025-05-27 11:05:34 -0500

    Pyshop evidencia 2

--- pyshop_3147246/db/models.py
+++ pyshop_3147246/db/models.py
@@ -1,11 +1,19 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 #crear clase
 
 class Categoria(Base):
     __tablename__="categorias"
-    id = Column(Integer,
-                PrimaryKey=True,
-                )
+    66
+    id = Column(Integer, primary_key=True,)
     nombre = Column(String(20))
+
+
+class Productos(Base):
+    __tablename__="Productos"
+    id = Column(Integer, primary_key=True,)
+    nombre = Column(String(20))
+    precio = Column(Integer)
+    categoria_id = Column(Integer , ForeignKey("categorias.id"))
+
